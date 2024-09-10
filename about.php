670ba29bd9a778430279e6d835d72d8d6547bba5
Nikita Priya <nikitapriya8862@gmail.com>
2024-09-10 14:24:14 +0530

    account

--- about.php
+++ about.php
@@ -1,103 +1,133 @@
 <head>
-<title> About :: HappyMarriageCenter</title>
+	<title> About :: HappyMarriageCenter</title>
 
-<?php
-include 'header.php' ;
-?>
+	<?php
+	include 'header.php';
+	?>
+	<script>
+	function CheckInputvalue() {
+		error = 0;
+		var vmobile = document.getElementById("mobile").value;
+		if (vmobile == "") {
+
+			document.getElementById("mobileresult").innerHTML = "<B><Font color=red>Mobile cannot be empty!</Font></B>";
+			return false;
+		} else {
+			//The content should contain only digits
+			var len = vmobile.length;
+			if (len != 10) {
+				document.getElementById("mobileresult").innerHTML = "<B><Font color=red>Mobile Number should be 10 digits!</Font></B>";
+				return false;
+			}
+
+			for (var i = 0; i < len; i++) {
+				var ch = vmobile.charAt(i); //9939802016
+				if (ch >= '0' && ch <= '9') {
+					//alert(ch + "allowed");
+					document.getElementById("mobileresult").innerHTML = "";
+				} else {
+					// alert(ch + "not allowed");
+					document.getElementById("mobileresult").innerHTML = "<B><Font color=red>Mobile can contain only digits!</Font></B>";
+					return false;
+				}
+			}
+		}
+	}
+</script>
 </head>
 <!-- breadcrumbs -->
 <div class="w3l_agileits_breadcrumbs">
-   <div class="container">
+	<div class="container">
 		<div class="w3l_agileits_breadcrumbs_inner">
 			<ul>
 				<li><a href="index.php">Home</a><span>&#187;</span></li>
-									
+
 				<li>About</li>
-				</ul>
+			</ul>
 		</div>
 	</div>
 </div>
 <!-- //breadcrumbs -->
 <!--team-->
-			<section class="w3ls-team text-center">
-				<div class="container">
-					<div class="tittle-agileinfo">
-						<h3>Meet our team</h3>
-					</div>
-					<div class="team-row">
-						<div class="col-md-3 w3ls-team-grids">
-							<h5>Clare</h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipi est eligendi scing elit consectetur.</p>
-							<div class="agileits-social">
-								<ul>
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-rss"></i></a></li>
-								</ul>
-							</div>
-							<div class="team-img">
-								<img src="images/team1.jpg" alt="">
-							</div>
-						</div>
-						<div class="col-md-3 w3ls-team-grids team-mdl">
-							<h5>Abelard</h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipi est eligendi scing elit consectetur.</p>
-							<div class="agileits-social">
-								<ul>
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-rss"></i></a></li>
-								</ul>
-							</div>
-							<div class="team-img">
-								<img src="images/team2.jpg" alt="">
-							</div>
-						</div>
-						<div class="col-md-3 w3ls-team-grids team-mdl1">
-							<h5>Daniel jack</h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipi est eligendi scing elit consectetur.</p>
-							<div class="agileits-social">
-								<ul>
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-rss"></i></a></li>
-								</ul>
-							</div>
-							<div class="team-img">
-								<img src="images/team4.jpg" alt="">
-							</div>
-						</div>
-						<div class="col-md-3 w3ls-team-grids">
-							<h5>Sally</h5>
-							<p>Lorem ipsum dolor sit amet, consectetur adipi est eligendi scing elit consectetur.</p>
-							<div class="agileits-social">
-								<ul>
-									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-									<li><a href="#"><i class="fa fa-rss"></i></a></li>
-								</ul>
-							</div>
-							<div class="team-img">
-								<img src="images/team3.jpg" alt="">
-							</div>
-						</div>
-						<div class="clearfix"> </div>
-					</div>
+<section class="w3ls-team text-center">
+	<div class="container">
+		<div class="tittle-agileinfo">
+			<h3>Meet our team</h3>
+		</div>
+		<div class="team-row">
+			<div class="col-md-3 w3ls-team-grids">
+				<h5>Clare</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipi est eligendi scing elit consectetur.</p>
+				<div class="agileits-social">
+					<ul>
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-rss"></i></a></li>
+					</ul>
 				</div>
-				</section>
-			<!--//team-->
+				<div class="team-img">
+					<img src="images/team1.jpg" alt="">
+				</div>
+			</div>
+			<div class="col-md-3 w3ls-team-grids team-mdl">
+				<h5>Abelard</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipi est eligendi scing elit consectetur.</p>
+				<div class="agileits-social">
+					<ul>
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-rss"></i></a></li>
+					</ul>
+				</div>
+				<div class="team-img">
+					<img src="images/team2.jpg" alt="">
+				</div>
+			</div>
+			<div class="col-md-3 w3ls-team-grids team-mdl1">
+				<h5>Daniel jack</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipi est eligendi scing elit consectetur.</p>
+				<div class="agileits-social">
+					<ul>
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-rss"></i></a></li>
+					</ul>
+				</div>
+				<div class="team-img">
+					<img src="images/team4.jpg" alt="">
+				</div>
+			</div>
+			<div class="col-md-3 w3ls-team-grids">
+				<h5>Sally</h5>
+				<p>Lorem ipsum dolor sit amet, consectetur adipi est eligendi scing elit consectetur.</p>
+				<div class="agileits-social">
+					<ul>
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-rss"></i></a></li>
+					</ul>
+				</div>
+				<div class="team-img">
+					<img src="images/team3.jpg" alt="">
+				</div>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+</section>
+<!--//team-->
 
 
 <!-- about -->
 <div class="about-w3layouts" id="about">
 	<div class="tittle-agileinfo">
-				<h3>About Us</h3>
-			</div>
+		<h3>About Us</h3>
+	</div>
 	<div class="about-left-agileits">
 		<div class="button">
 			<a href="#small-dialog" class="play-icon popup-with-zoom-anim"><span class="glyphicon glyphicon-play-circle" aria-hidden="true"></span></a>
 		</div>
-		<div id="small-dialog" class="mfp-hide w3ls_small_dialog wthree_pop">		
+		<div id="small-dialog" class="mfp-hide w3ls_small_dialog wthree_pop">
 			<div class="agileits_modal_body">
 				<iframe src="https://player.vimeo.com/video/198296034"></iframe>
 			</div>
@@ -111,90 +141,102 @@ include 'header.php' ;
 	<div class="clearfix"> </div>
 </div>
 <!-- //about-bottom -->
- <!--search-section -->
- <div class="search-wthree">
- <div class="container">
-	<h3 class="subheading-agileits-w3layouts">Start your journey with confidence.</h3>
-	<div class="sim-button button12"><a href="matches.php">Search Now</a></div>
-	<div class="clearfix"> </div>
- </div>
-  </div>
-  <!--//search-section -->
- <!--middle-section -->
+<!--search-section -->
+<div class="search-wthree">
+	<div class="container">
+		<h3 class="subheading-agileits-w3layouts">Start your journey with confidence.</h3>
+		<div class="sim-button button12"><a href="matches.php">Search Now</a></div>
+		<div class="clearfix"> </div>
+	</div>
+</div>
+<!--//search-section -->
+<!--middle-section -->
 <div class="middle-section-agileits-w3layouts">
-<div class="container">
+	<div class="container">
 		<h3 class="subheading-agileits-w3layouts"><span>Elite </span>Match</h3>
 		<p class="para-w3layouts">Exclusive Matchmaking Service for the Elite</p>
 		<div class="sim-button button12"><a href="contact.php">Contact Us</a></div>
-</div>	
+	</div>
 </div>
- <!--//middle-section -->
+<!--//middle-section -->
 <!-- services -->
-	<div class="services" id="services">
-		<div class="container">
+<div class="services" id="services">
+	<div class="container">
 		<div class="tittle-agileinfo">
-				<h3>Assisted Services</h3>
+			<h3>Assisted Services</h3>
+		</div>
+		<div class="col-md-6 w3_agileits_services_grids">
+			<div class="w3_agileits_services_grid">
+				<div class="w3_agileits_services_grid_agile">
+					<div class="w3_agileits_services_grid_1">
+						<i class="fa fa-search" aria-hidden="true"></i>
+					</div>
+					<h3>Search within reach</h3>
+				</div>
 			</div>
-			<div class="col-md-6 w3_agileits_services_grids">
-				<div class="w3_agileits_services_grid">
-					<div class="w3_agileits_services_grid_agile">
-						<div class="w3_agileits_services_grid_1">
-							<i class="fa fa-search" aria-hidden="true"></i>
-						</div>
-						<h3>Search within reach</h3>
+			<div class="w3_agileits_services_grid">
+				<div class="w3_agileits_services_grid_agile">
+					<div class="w3_agileits_services_grid_1">
+						<i class="fa fa-list-alt" aria-hidden="true"></i>
 					</div>
+					<h3>Shortlisted matches</h3>
 				</div>
-				<div class="w3_agileits_services_grid">
-					<div class="w3_agileits_services_grid_agile">
-						<div class="w3_agileits_services_grid_1">
-							<i class="fa fa-list-alt" aria-hidden="true"></i>
-						</div>
-						<h3>Shortlisted matches</h3>
-					</div>
-				</div>
-				<div class="w3_agileits_services_grid">
-					<div class="w3_agileits_services_grid_agile">
-						<div class="w3_agileits_services_grid_1">
-							<i class="fa fa-phone" aria-hidden="true"></i>
-						</div>
-						<h3>Initiate communication</h3>
-					</div>
-				</div>
-				<div class="w3_agileits_services_grid">
-					<div class="w3_agileits_services_grid_agile">
-						<div class="w3_agileits_services_grid_1">
-							<i class="fa fa-users" aria-hidden="true"></i>
-						</div>
-						<h3>Meet the prospects</h3>
-					</div>
-				</div>
-				<div class="clearfix"> </div>
 			</div>
-			<div class="col-md-6 regstr-r-w3-agileits">
+			<div class="w3_agileits_services_grid">
+				<div class="w3_agileits_services_grid_agile">
+					<div class="w3_agileits_services_grid_1">
+						<i class="fa fa-phone" aria-hidden="true"></i>
+					</div>
+					<h3>Initiate communication</h3>
+				</div>
+			</div>
+			<div class="w3_agileits_services_grid">
+				<div class="w3_agileits_services_grid_agile">
+					<div class="w3_agileits_services_grid_1">
+						<i class="fa fa-users" aria-hidden="true"></i>
+					</div>
+					<h3>Meet the prospects</h3>
+				</div>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+		<div class="col-md-6 regstr-r-w3-agileits">
 			<div class="form-bg-w3ls">
 				<h3 class="subhead-agileits white-w3ls">Interested in assisted services....?</h3>
 				<p class="para-agileits-w3layouts">Leave your details below, we'll call you back.</p>
-				<form action="#" method="post">
+				<form method="post" onsubmit="return CheckInputvalue()">
 					<input type="text" name="Name" placeholder="Full name" required="">
-					<input type="text" name="number" placeholder="Number" required="">
+					<input type="text" name="Mobile" placeholder="Mobile Number" required="" id="mobile"> <span id="mobileresult"></span>
 					<input type="email" name="Email" placeholder="Email" required="">
-					<select class="form-control">
-						<option>Select Location</option>
-						<option>Bangalore</option>
-						<option>Chandigarh</option>
-						<option>Kolkata</option>
-						<option>Ludhiana</option>
-						<option>Mumbai</option>
-						<option>Mysore</option>
-						<option>Pune</option>
-					</select>
-					<input type="submit" value="Submit" class="button-w3layouts hvr-rectangle-out">
-				</form>	
-			</div>
+					<textarea name="Address" placeholder="Enter your Address" required=""></textarea>
+					<input type="submit" value="Submit" name="submit" class="button-w3layouts hvr-rectangle-out">
+				</form>
 			</div>
 		</div>
 	</div>
+</div>
 <!-- //services -->
 <?php
-include 'footer.php' ;
+include 'config.php';
+
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['Name'];
+	$mobile = $_POST['Mobile'];
+	$email = $_POST['Email'];
+	$address = $_POST['Address'];
+	$sql = "INSERT INTO `services`( `Name`, `Mobile`, `Email`, `Address`) VALUES ('$name','$mobile','$email','$address')";
+	//echo  $sql;
+	if ($conn->query($sql)) {
+		echo "<script>
+                   alert('Your details seved successfully!We will contact you shortly'); 
+              </script>";
+	} else {
+		echo "<BR><BR> Error occured";
+	}
+}
+
+?>
+<?php
+include 'footer.php';
 ?>
