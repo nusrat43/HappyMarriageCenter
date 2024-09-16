d9b98a976df57fb669d0a9114fd282f5dab72d7b
Nikita Priya <nikitapriya8862@gmail.com>
2024-09-16 16:33:27 +0530

    change

--- register.php
+++ register.php
@@ -16,11 +16,26 @@
 				a = 0;
 			} else {
 				document.getElementById("pass").type = "password";
-				document.getElementById("eye").src="images/eye-solid.svg";
+				document.getElementById("eye").src = "images/eye-solid.svg";
 				a = 1;
 			}
 		}
 	</script>
+	<style>
+		.login h3,
+		.signup h3 {
+			margin: 0.4em 0;
+			color: #a63d56;
+		}
+
+		.login-right-info {
+			padding: 1.5em;
+		}
+
+		h4 {
+			margin: 0.6em 0;
+		}
+	</style>
 </head>
 <!-- breadcrumbs -->
 <div class="w3l_agileits_breadcrumbs">
@@ -103,14 +118,14 @@
 				<div class="w3_form_body_grid w3_form_body_grid1">
 					<span>Password</span>
 
-					<div style="display: flex; align-items: center;"  >
+					<div style="display: flex; align-items: center;">
 						<input type="password" placeholder="Password" style="flex: 1;" id="pass" name="Password">
-						<img  onclick="visible()"  style="margin-left: -25px; margin-bottom :6px;" id="eye" src="images/eye-solid.svg"></img>
+						<img onclick="visible()" style="margin-left: -25px; margin-bottom :6px;" id="eye" src="images/eye-solid.svg"></img>
 					</div>
 				</div>
 				<div class="w3_form_body_grid w3_form_body_grid1">
 					<span>profile Photo</span>
-					<input type="file" name="fileToUpload"  required="">
+					<input type="file" name="fileToUpload" required="">
 				</div>
 				<input type="submit" value="Sign Up" name="submit">
 			</form>
@@ -135,8 +150,9 @@
 			</div>
 		</div>
 		<div class="col-md-4 login-right-info">
-			<h3 class="subhead-agileits">Why Join Elite Match</h3>
-			<p class="para-agileits-w3layouts">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			<h2 class="subhead-agileits" style="color: goldenrod;text-align:center;"><b>Gold Membership</b></h2>
+			<h3 align=center> Charges : 8000 &#8377;</h3>
+			<h4 align=center>Duration : 3 Years</h4>
 			<ul>
 				<li><i class="fa fa-check" aria-hidden="true"></i>Lorem ipsum</li>
 				<li><i class="fa fa-check" aria-hidden="true"></i>Vivamus lacus</li>
@@ -147,12 +163,17 @@
 			<div class="sim-button button12"><a href="login.php">Login</a></div>
 		</div>
 		<div class="col-md-4 login-right-info right-info-find">
-			<h3 class="subhead-agileits">Easy steps to find your life partner</h3>
+			<h2 class="subhead-agileits" style="color:cornflowerblue;text-align:center;"><b>Silver Membership</b></h2>
+			<h3 align=center>Charges : 5000 &#8377;</h3>
+			<h4 align=center>Duration : 2 Years</h4>
 			<ul>
-				<li><i class="fa fa-ellipsis-v" aria-hidden="true"></i>Vivamus lacus nisl, suscipit in vehicula sit amet, sollicitudin eget eros.</li>
-				<li><i class="fa fa-ellipsis-v" aria-hidden="true"></i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet.</li>
-				<li><i class="fa fa-ellipsis-v" aria-hidden="true"></i>Vivamus lacus nisl, suscipit in vehicula sit amet, sollicitudin eget eros.</li>
+				<li><i class="fa fa-check" aria-hidden="true"></i>Lorem ipsum</li>
+				<li><i class="fa fa-check" aria-hidden="true"></i>Vivamus lacus</li>
+				<li><i class="fa fa-check" aria-hidden="true"></i>quisquam est</li>
+				<li><i class="fa fa-check" aria-hidden="true"></i>Pellentesque</li>
 			</ul>
+			<h5>Already a member....<i class="fa fa-hand-o-down" aria-hidden="true"></i></h5>
+			<div class="sim-button button12"><a href="login.php">Login</a></div>
 		</div>
 	</div>
 </div>
@@ -165,46 +186,46 @@ include 'config.php';
 
 if (isset($_POST['submit'])) {
 
-	 /////////////////Phot Upload Code
+	/////////////////Phot Upload Code
 
-	 $target_dir = "uploads/"; // Specify the directory where the file will be uploaded
-	 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-	 $uploadOk = 1;
-	 $fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+	$target_dir = "uploads/"; // Specify the directory where the file will be uploaded
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$fileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-	 // Check if file already exists
-	 if (file_exists($target_file)) {
+	// Check if file already exists
+	if (file_exists($target_file)) {
 		echo "Sorry, file already exists.";
 		$uploadOk = 0;
-	 }
+	}
 
-	 // Check file size (limit to 5MB)
-	 if ($_FILES["fileToUpload"]["size"] > 5000000) {
+	// Check file size (limit to 5MB)
+	if ($_FILES["fileToUpload"]["size"] > 5000000) {
 		echo "Sorry, your file is too large.";
 		$uploadOk = 0;
-	 }
+	}
 
-	 // Allow certain file formats
-	 $allowedTypes = array("jpg", "png", "jpeg", "gif");
-	 if (!in_array($fileType, $allowedTypes)) {
+	// Allow certain file formats
+	$allowedTypes = array("jpg", "png", "jpeg", "gif");
+	if (!in_array($fileType, $allowedTypes)) {
 		echo "Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed.";
 		$uploadOk = 0;
-	 }
+	}
 
-	 // Check if $uploadOk is set to 0 by an error
-	 if ($uploadOk == 0) {
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
 		echo "Sorry, your file was not uploaded.";
-	 } else {
+	} else {
 		// If everything is ok, try to upload the file
 		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-		   echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+			echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
 		} else {
-		   echo "Sorry, there was an error uploading your file.";
+			echo "Sorry, there was an error uploading your file.";
 		}
-	 }
+	}
 
 
-	$profile_for=$_POST['Profile_for'];
+	$profile_for = $_POST['Profile_for'];
 	$name = $_POST['Name'];
 	$gender = $_POST['Gender'];
 	$Date_of_birth = $_POST['DOB'];
@@ -212,10 +233,10 @@ if (isset($_POST['submit'])) {
 	$country = $_POST['Country'];
 	$email = $_POST['Email']; //normal variable can be accessed only in one webpage
 	$password = md5($_POST['Password']);
-	$sql="INSERT INTO `register`(`Profile_For`, `Name`, `Gender`, `D_O_B`, `Religion`, `Country`, `Email`, `Password`,`Profile_Pic`) VALUES ('$profile_for','$name','$gender','$Date_of_birth','$religion','$country','$email','$password','$target_file')";
+	$sql = "INSERT INTO `register`(`Profile_For`, `Name`, `Gender`, `D_O_B`, `Religion`, `Country`, `Email`, `Password`,`Profile_Pic`) VALUES ('$profile_for','$name','$gender','$Date_of_birth','$religion','$country','$email','$password','$target_file')";
 	//echo  $sql;
 
-	
+
 
 
 	if ($conn->query($sql)) {
